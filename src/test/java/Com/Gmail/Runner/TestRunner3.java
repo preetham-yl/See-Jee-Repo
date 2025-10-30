d478731ed3c9df6d4150a1d4f70e384943715f24
preetham <preethamlgh@gmail.com>
2025-10-30 11:45:41 +0530

    selenium changes

---
+++ src/test/java/Com/Gmail/Runner/TestRunner3.java
@@ -0,0 +1,16 @@
+package Com.Gmail.Runner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+    features = ".\\src\\test\\java\\Com\\Gmail\\Users\\Features\\logintable.feature",
+    glue = "Com.Gmail.Users.StepDef",
+    //tags = "@Tc1 , @Tc2"
+    // tags = "@Tc1 and @Tc2"
+    // tags = ("@Tc1 or @Tc2")
+    plugin = {"pretty", "html:Reports/cucumber-reports.html"}
+)
+public class TestRunner3 extends AbstractTestNGCucumberTests
+{
+}
